c7c62865026fa02638f685af4955ba5568e97f95
azer7777 <azertyuio123a77@gmail.com>
2023-11-07 05:16:05 +0100

    update

--- lettings/views.py
+++ lettings/views.py
@@ -3,14 +3,9 @@ from .models import Letting
 
 
 def index(request):
-    return render(request, 'index.html')
-
-
-def lettings_index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
-
+    return render(request, 'lettings/index.html', context)
 
 
 def letting(request, letting_id):
@@ -19,6 +14,6 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)
 
 
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('', include('lettings.urls')),  # Include new 'lettings' app URLs
     path('', include('profiles.urls')),  # Include new 'profiles' app URLs
     path('admin/', admin.site.urls),
