6caedcc49123f18328eb3511966490ae67c2b182
azer7777 <azertyuio123a77@gmail.com>
2023-11-24 04:48:41 +0100

    404 500

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls import handler404, handler500
 from . import views
+
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -9,3 +10,6 @@ urlpatterns = [
     path("lettings/", include("lettings.urls", namespace="lettings")),
     path("profiles/", include("profiles.urls", namespace="profiles")),
 ]
+
+handler404 = views.custom_404
+handler500 = views.custom_500
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+
+def custom_404(request, exception):
+    return render(request, "404.html", status=404)
+
+
+def custom_500(request):
+    return render(request, "500.html", status=500)
