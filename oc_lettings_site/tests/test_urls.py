458947a48f8645a0173c341d875e15cf34c5547e
azer7777 <azertyuio123a77@gmail.com>
2023-11-30 09:37:29 +0100

    complete tests

--- oc_lettings_site/tests/test_urls.py
+++ oc_lettings_site/tests/test_urls.py
@@ -1,1 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
 
+
+class UrlsTest(TestCase):
+    def test_index_url(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_lettings_namespace_url(self):
+        response = self.client.get(reverse("lettings:index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_profiles_namespace_url(self):
+        response = self.client.get(reverse("profiles:index"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_custom_404_handler(self):
+        response = self.client.get("/nonexistent-page/")
+        self.assertEqual(response.status_code, 404)
--- oc_lettings_site/tests/test_views.py
+++ oc_lettings_site/tests/test_views.py
@@ -1,2 +1,29 @@
+from django.test import Client
+from django.urls import reverse
+import pytest
 
 
+@pytest.fixture
+def client():
+    return Client()
+
+
+def test_index_view(client):
+    url = reverse("index")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert "Welcome to Holiday Homes" in response.content.decode()
+
+
+def test_custom_404_view(client):
+    # Simulate a server error
+    with pytest.raises(Exception):
+        url = reverse("custom_404")
+        client.get(url)
+
+
+def test_custom_500_view(client):
+    # Simulate a server error
+    with pytest.raises(Exception):
+        url = reverse("custom_500")
+        client.get(url)
