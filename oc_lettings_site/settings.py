f626f646456cd8b1c2275f78c8974128c2364b04
azer7777 <azertyuio123a77@gmail.com>
2023-12-17 06:21:18 +0100

    update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,6 @@ SENTRY_DSN = os.environ.get("SENTRY_DSN", default="")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", default="")
-print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
