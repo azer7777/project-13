7c890027981650b01bf4baf1da66fb819343338a
azer7777 <azertyuio123a77@gmail.com>
2023-12-17 05:54:20 +0100

    update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,7 +15,8 @@ SENTRY_DSN = os.environ.get("SENTRY_DSN", default="")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("THE_SECRET_KEY", default="")
+SECRET_KEY = os.environ.get("SECRET_KEY", default="")
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
